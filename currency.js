b17b6d675b440b75438557bece673bd2e8cf5fe5
atisproj <egorklkve@yandex.ru>
2023-12-22 21:27:39 +0800

    4commit

---
+++ currency.js
@@ -0,0 +1,22 @@
+let cuurencyList = {
+    'RUBCNY': 'B1',
+    'RUBBTC': 'B2',
+    'RUBUSDT': 'B3',
+    'RUBETH': 'B4',
+    'CNYRUB': 'B5',
+    'CNYUSDT': 'B6',
+    'BTCRUB': 'B7',
+    'BTCCNY': 'B8',
+    'BTCUSDT': 'B9',
+    'BTCETH': 'B10',
+    'USDTRUB': 'B11',
+    'USDTCNY': 'B12',
+    'USDTETH': 'B13',
+    'USDTBTC': 'B14',
+    'ETHRUB': 'B15',
+    'ETHCNY': 'B16',
+    'ETHUSDT': 'B17',
+    'ETHBTC': 'B18',
+}
+
+// https://sheets.googleapis.com/v4/spreadsheets/15dKus_k2TI23_sBnYQjWOfACfijZsk7xJ4CsVMFLMMs/values/B2?key=AIzaSyB-dszV5PwFcT5yXw-ZQ5dwA3l4YCMfeJE
